9587c739977cf1aec80185eafc34e7271a862ca8
Vlad <foxyfo1703@gmail.com>
2020-10-03 11:57:58 +0300

    changed config

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ app.use("/api/link", require("./routes/link.routes"))
 app.use("/t", require("./routes/redirect.routes"))
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")))
+  // app.use("/", express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static("client/build"))
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
 
-const PORT = process.env.PORT || config.get("port") || 5000
+const PORT = process.env.PORT || config.get("port") || 8080
 
 async function start() {
   try {
